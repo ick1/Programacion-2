d350778bf5d44299d1aec0d60f5a15fda1153f76
Ricardo Dominguez Ojeda <dominguezojedaricado@gmail.com>
2024-09-01 00:59:11 -0600

    Agregando nuevas funciones, aun por terminar

--- Fecha/Fecha.java
+++ Fecha/Fecha.java
@@ -46,9 +46,41 @@ public class Fecha {
         }
     }
 
+    private int diasMes(int mm){
+        switch (mm) {
+            case 4: case 6: case 9: case 11:
+                return 30;
+            case 2:
+                if(bisiesto()){
+                    return 29; 
+                } else {
+                     return 28; 
+                }
+            default:
+                return 31;
+        }
+    }
+
     private void valida(){
-        if(day<1 || day>31){
-            day = 1;
+        switch (month) {
+            case 4: case 6: case 9: case 11:
+                if(day<1 || day>30){
+                    day = 1; 
+                }
+            case 2:
+                if(bisiesto()){
+                    if(day<1 || day>29){
+                        day = 1; 
+                    } 
+                } else {
+                    if(day<1 || day>28){
+                        day = 1;
+                    }
+                }
+            default:
+                if(day<1 || day>31){
+                    day = 1; 
+                }
         }
         if(month<1 || month>12){
             month = 1;
@@ -56,5 +88,140 @@ public class Fecha {
         if(year<1900 || year>2050){ 
             year = 1900; 
         }
+    }
+
+    /* Getters */
+
+    public int getDay(){
+        return day; 
+    }
+
+    public int getMonth(){
+        return month; 
+    }
+
+    public int getYear(){
+        return year; 
+    }
+
+    /* Setters */
+
+    public void setDay(int dd){
+        day = dd; 
+    }
+
+    public void setMonth(int mm){
+        month = mm; 
+    }
+
+    public void setYear(int yy){
+        year = yy; 
+    }
+
+    public void corta(){
+        System.out.println(day+"-"+month+"-"+year);
+    }
+
+    public int diasTranscurridos(){
+        int db = 365; 
+        if(bisiesto()){
+            db = 366; 
+        }
+        return (day-1)+((month-1)*diasMes(month-1))+((year-1900)*db);
+    }
+
+    /* Congruencia de Zeller*/
+
+    public int diaSemana(){
+        int mm=month, yy=year, v1, v2; 
+
+        //La congruencia denomina a Enero y Febrero como el mes 13 y 14 del año anterior
+        if (mm <= 2) {
+            mm += 12; 
+            yy -= 1; 
+        }
+
+        //Calculando variables necesarias
+        v1 = yy % 100; 
+        v2 = yy / 100;
+
+        // Aplicacion la congruencia de Zeller y ajustando para retornar 0-domingo ... 6-sabado
+        return (((day + (13 * (mm + 1)) / 5 + v1 + v1 / 4 + v2 / 4 + 5 * v2) % 7) + 6) % 7;
+    }
+
+    public void larga(){
+        String n1=null, n2=null;
+        switch(diaSemana()){
+            case 0: 
+                n1 = "Domingo";
+                break; 
+            case 1: 
+                n1 = "Lunes";
+                break;
+            case 2: 
+                n1 = "Martes";
+                break; 
+            case 3: 
+                n1 = "Miercoles";  
+                break; 
+            case 4:
+                n1 = "Jueves"; 
+                break; 
+            case 5: 
+                n1 = "Viernes";
+                break; 
+            case 6: 
+                n1 = "Sabado"; 
+                break;  
+            default: 
+                break;     
+        } 
+
+        switch(month){
+            case 1: 
+                n2 = "enero"; 
+                break; 
+            case 2: 
+                n2 = "febrero"; 
+                break; 
+            case 3: 
+                n2 = "marzo"; 
+                break; 
+            case 4: 
+                n2 = "abril"; 
+                break; 
+            case 5: 
+                n2 = "mayo"; 
+                break;
+            case 6: 
+                n2 = "junio"; 
+                break;
+            case 7: 
+                n2 = "julio"; 
+                break;   
+            case 8: 
+                n2 = "agosto"; 
+                break; 
+            case 9: 
+                n2 = "septiembre"; 
+                break; 
+            case 10: 
+                n2 = "octubre"; 
+                break; 
+            case 11: 
+                n2 = "noviembre"; 
+                break; 
+            case 12: 
+                n2 = "diciembre"; 
+                break; 
+            default: 
+                break; 
+        }
+
+        System.out.println(n1 + " " + day + " de " + n2 + " de "+ year);
+    }
+
+    public void fechaTras(int n){
+
     }
 }
--- Fecha/Principal.java
+++ Fecha/Principal.java
@@ -10,6 +10,16 @@ public class Principal {
     static int dd,mm,yy;
 
     public static void main(String[] args) {
-        System.out.println(f1.diasMes());
+        f1.setDay(39);
+        f1.setMonth(2);
+        f1.setYear(2003);
+        System.out.println(f1.diasTranscurridos());
+        System.out.println(f1.diaSemana());
+        /*if (f1.bisiesto()){
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }*/
+        f1.larga();
     }
 }

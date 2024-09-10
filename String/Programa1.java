a7d86363419b91a0212a4af319c500fb2ffcc78f
Ricardo Dominguez Ojeda <dominguezojedaricado@gmail.com>
2024-09-09 19:47:47 -0600

    Utilizando nuevos metodos de String y documentando cada accion

--- String/Programa1.java
+++ String/Programa1.java
@@ -7,14 +7,23 @@ public class Programa1 {
     static Scanner leer = new Scanner(System.in); 
 
     public static void main(String[] args) {
-        System.out.println("Favor de ingresar una oracion: ");
-        cadena = leer.nextLine();
-        System.out.println("\nIndicar la palabra a remplazar: ");
-        pal1 = leer.nextLine(); 
-        System.out.println("\nIndicar la nueva palabra a colocar: ");
-        pal2 = leer.nextLine(); 
+        //Colocar la cadena (oracion) a ocupar
+        System.out.print("Favor de ingresar una oracion: ");
+        cadena = leer.nextLine().toLowerCase().trim();
 
+        //Solicitar la subcadena a cambiar 
+        System.out.println("\nIndicar la palabra a remplazar: ");
+        pal1 = leer.nextLine().toLowerCase().trim();
+        
+        //Solicitar la nueva subcadena
+        System.out.println("\nIndicar la nueva palabra a colocar: "); 
+        pal2 = leer.nextLine().toLowerCase().trim(); 
+
+        //Remplazar la subcadena anterior con la nueva
         cadena = cadena.replace(pal1, pal2);
+
+        //Convertir la primera letra de la oracion en mayuscula y mostrar en pantalla la nueva cadena
+        cadena = cadena.substring(0, 1).toUpperCase() + cadena.substring(1); 
         System.out.println("\nLa nueva cadena es: " + cadena);
     } 
 }
